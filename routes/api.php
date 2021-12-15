3bad23b15d93d4d4e7d800d1076b40456fd04170
LordHob <rafaelginergomez@gmail.com>
2021-12-15 19:17:54 +0100

    Añado endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\MembershipController;
+use App\Http\Controllers\FriendshipController;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\PartyController;
+use App\Http\Controllers\PassportController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +21,55 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('signUp', [PassportController::class, 'signUp']);
+Route::post('signIn', [PassportController::class, 'signIn']);
+
+Route::middleware('auth:api')->group(function () {
+
+    // Users table Endpoints
+
+    Route::post('logout', [PassportController::class, 'logout']);
+    Route::get('getAll', [UserController::class, 'getAll']);
+    Route::post('getProfile', [UserController::class, 'getProfile']);
+    Route::put('updateProfile', [UserController::class, 'updateProfile']);
+    Route::delete('removeUser', [UserController::class, 'removeUser']);
+
+    // Friends table Endpoints
+
+    Route::post('addFriend', [FriendshipController::class, 'addFriend']);
+    Route::post('getFriends', [FriendshipController::class, 'getFriends']);
+    Route::post('getPendingFriends', [FriendshipController::class, 'getPendingFriends']);
+    Route::post('getPendingFriendRequest', [FriendshipController::class, 'getPendingFriendRequest']);
+    Route::put('acceptFriend', [FriendshipController::class, 'acceptFriend']);
+    Route::delete('removeFriend', [FriendshipController::class, 'removeFriend']);
+
+    // Games table Endpoints
+
+    Route::post('addGame', [GameController::class, 'addGame']);
+    Route::get('getGames', [GameController::class, 'getGames']);
+    Route::post('getGameById', [GameController::class, 'getGameById']);
+    Route::post('getGameByTitle', [GameController::class, 'getGameByTitle']);
+    Route::put('updateGame', [GameController::class, 'updateGame']);
+    Route::delete('removeGame', [GameController::class, 'removeGame']);
+
+    // Parties table Endpoints  
+
+    Route::post('addParty', [PartyController::class, 'addParty']);
+    Route::post('joinParty', [PartyController::class, 'joinParty']);
+    Route::get('getParties', [PartyController::class, 'getParties']);
+    Route::post('getPartiesByGameId', [PartyController::class, 'getPartiesByGameId']);
+    Route::post('getPartiesByGameTitle', [PartyController::class, 'getPartiesByGameTitle']);
+    Route::post('getMyParties', [PartyController::class, 'getMyParties']);
+    Route::post('getPartyMembers', [PartyController::class, 'getPartyMembers']);
+    Route::delete('quitParty', [PartyController::class, 'quitParty']);
+    Route::delete('kickFromParty', [PartyController::class, 'kickFromParty']);
+    Route::delete('removeParty', [PartyController::class, 'removeParty']);
+
+    // Messages table Endpoints
+
+    Route::get('getMessages', [MessageController::class, 'getMessages']);
+    Route::post('addMessage', [MessageController::class, 'addMessage']);
+    Route::put('updateMessage', [MessageController::class, 'updateMessage']);
+    Route::delete('removeMessage', [MessageController::class, 'removeMessage']);
+    Route::post('getPartyMessages', [MessageController::class, 'getPartyMessages']);
 });
